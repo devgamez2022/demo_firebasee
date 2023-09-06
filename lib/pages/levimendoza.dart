a537f2dc68e991ad0846f4f5af406bb35ec7b976
levi1405 <108413228+levi1405@users.noreply.github.com>
2023-09-06 10:38:09 -0600

    Adicion de Levi

---
+++ lib/pages/levimendoza.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeviMendoza extends StatelessWidget {
+  const LeviMendoza({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
