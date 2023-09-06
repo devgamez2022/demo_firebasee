2eb267facef1dacd5be0e99d1513a1205ab4823c
adonay2003 <108413232+adonay2003@users.noreply.github.com>
2023-09-06 10:47:47 -0600

    Adicion Gerson

---
+++ lib/pages/gersoncampos.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class gersoncampos extends StatelessWidget {
+  const gersoncampos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
