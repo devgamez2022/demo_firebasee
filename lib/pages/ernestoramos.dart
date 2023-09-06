09f96569e7874c3db929027323c21cd04708c262
JEHRErnesto <ernestoramos1721@gmail.com>
2023-09-06 10:51:02 -0600

    Adición Ernesto

---
+++ lib/pages/ernestoramos.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ErnestoRamos extends StatelessWidget {
+  const ErnestoRamos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
