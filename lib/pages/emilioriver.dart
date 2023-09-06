3c42533e25345f4e5bc96f7c093876e309ed68af
kathyaclimaco <108421513+kathyaclimaco@users.noreply.github.com>
2023-09-06 10:44:36 -0600

    Adiccion de emilio rivera

---
+++ lib/pages/emilioriver.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class emilioriver extends StatelessWidget {
+  const emilioriver({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
